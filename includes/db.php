fda651b81932cad9a7cc819702a3c05b4c758957
Enyi <enyilio@liuenyide-MacBook-Pro.local>
2017-12-07 20:29:36 +0800

    add includes folder

--- includes/db.php
+++ includes/db.php
@@ -3,8 +3,8 @@
 	//PHP-MySQL 是 PHP 操作 MySQL 資料庫最原始的 Extension ，PHP-MySQLi 的 i 代表 Improvement ，提更了相對進階的功能，就 Extension 而言，本身也增加了安全性。
 
 	$db['db_host'] = 'localhost';
-	$db['db_user'] = 'enyilio';
-	$db['db_password'] = '6666';
+	$db['db_user'] = 'root';
+	$db['db_password'] = '';
 	$db['db_name'] = 'CMS-Practice';
 
 	foreach ($db as $key => $value) {
